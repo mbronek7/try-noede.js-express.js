863b79d495d2b5bc2e7b1989363afc68881eabf7
mbronek7 <mbronek7@gmail.com>
2018-12-30 03:54:05 +0100

    Add layout

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var ejsLayouts = require("express-ejs-layouts");
+var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 
@@ -17,6 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+
+app.use(ejsLayouts);
 
 app.use('/', indexRouter);
 
